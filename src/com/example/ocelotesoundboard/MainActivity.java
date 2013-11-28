3570b7baf94e06ec0b2e7c7a92daa9b0aa2d52a2
Thibault Lelievre <thibault.lelievre@visionobjects.com>
2013-11-28 14:33:48 +0100

    add assets management

--- src/com/example/ocelotesoundboard/MainActivity.java
+++ src/com/example/ocelotesoundboard/MainActivity.java
@@ -11,16 +11,31 @@ import android.os.Environment;
 import android.app.Activity;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
+import android.text.Layout;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "MyActivity";
+	private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i=0; i<10;i++){
+        	Button b = new Button(this);
+        	b.setText(Integer.toString(i));
+        }
+        button = new Button(this);
+        button.setText("heelo");
+        ((ViewGroup)findViewById(R.id.layout)).addView(button);
+        
         File file;
         File soundDirectory = new File(Environment.getDataDirectory()+File.separator+"sounds");
         FilenameFilter filter = new FilenameFilter() {
