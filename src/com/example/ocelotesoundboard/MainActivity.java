45deaffa40f78fdcc3eda644556e2ff97ca300b0
Thibault Lelievre <thibault.lelievre@visionobjects.com>
2013-12-13 14:16:23 +0100

    add media player and remove binaries

--- src/com/example/ocelotesoundboard/MainActivity.java
+++ src/com/example/ocelotesoundboard/MainActivity.java
@@ -5,10 +5,12 @@ import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.text.Layout;
@@ -24,17 +26,18 @@ import android.view.View.OnClickListener;
 public class MainActivity extends Activity {
 	private static final String TAG = "MyActivity";
 	private Button button;
+	private String[] list = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for(int i=0; i<10;i++){
-        	Button b = new Button(this);
-        	b.setText(Integer.toString(i));
-        }
-        button = new Button(this);
-        button.setText("heelo");
-        ((ViewGroup)findViewById(R.id.layout)).addView(button);
+//        for(int i=0; i<10;i++){
+//        	Button b = new Button(this);
+//        	b.setText(Integer.toString(i));
+//        }
+//        button = new Button(this);
+//        button.setText("heelo");
+//        ((ViewGroup)findViewById(R.id.layout)).addView(button);
         
         File file;
         File soundDirectory = new File(Environment.getDataDirectory()+File.separator+"sounds");
@@ -52,7 +55,6 @@ public class MainActivity extends Activity {
         }
         
         AssetManager am = getAssets();
-        String[] list = null;
         try {
 			list = am.list("");
 		} catch (IOException e) {
@@ -62,8 +64,19 @@ public class MainActivity extends Activity {
         
         for(int i =0; i<list.length; i++){
         	if(list[i].endsWith(".mp3") || list[i].endsWith(".ogg")){
-        		Button b1 = new Button(this);
-        		b1.setText("Button");
+        		Button button = new Button(this);
+        		button.setId(i);
+        		String bLabel = list[i].replaceAll("_", " ");
+        		bLabel = bLabel.substring(0, bLabel.lastIndexOf('.'));
+        		button.setText(bLabel);
+        		button.setOnClickListener(new View.OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						playSound((Button) v);
+					}
+				});
+                ((ViewGroup)findViewById(R.id.layout)).addView(button);
         	}
         }
 //        //use audio resources
@@ -73,6 +86,19 @@ public class MainActivity extends Activity {
 //        for(final File fileEntry : soundDirectory.listFiles(filter)){
 //        	//create button for this file
 //        }
+    }
+    
+    public void playSound(Button button){
+        try {
+            AssetFileDescriptor afd = getAssets().openFd(list[button.getId()]);
+            MediaPlayer player = new MediaPlayer();
+            player.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+            player.prepare();
+            player.start();
+            } 
+        catch (IllegalArgumentException e) {    } 
+        catch (IllegalStateException e) { } 
+        catch (IOException e) { } 	
     }
 
 
